46cf8b2573198c3bba9c57d4859e3259a87c9a89
igoreta <ivoronline@gmail.com>
2021-01-13 12:52:12 +0100

    Initial commit

---
+++ src/main/java/com/ivoronline/profiles_applicationproperties/ProfilesApplicationpropertiesApplication.java
@@ -0,0 +1,13 @@
+package com.ivoronline.profiles_applicationproperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfilesApplicationpropertiesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ProfilesApplicationpropertiesApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ivoronline/profiles_applicationproperties/ProfilesApplicationpropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivoronline.profiles_applicationproperties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfilesApplicationpropertiesApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
